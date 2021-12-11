55f70fc50a32a5c9daf517000717deb327745eb9
Vinicius <fsvinicius9@gmail.com>
2021-12-11 17:35:10 -0300

    teste

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
 Route::prefix('app')->group(function () {
