ca0c326ca74b6e264ef9313c101462dc61a04b3c
Vinicius <fsvinicius9@gmail.com>
2022-06-05 15:19:34 -0300

    Customizando mensagens de erros

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -12,18 +12,33 @@ class ContatoController extends Controller
     {
         $motivo_contatos = MotivoContato::all();
 
-        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contato' => $motivo_contatos]);
+        return view(
+            'site.contato',
+            ['titulo' => 'Contato (teste)', 'motivo_contato' => $motivo_contatos]
+        );
     }
 
     public function salvar(Request $request)
     {
-        $request->validate([
+        $regras = [
             'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
+        $feedback =  [
+            'nome.required' => 'O campo nome precisa ser preenchido', //prioritario em relação a generica
+            'email.email' => 'O email informado não é válido',
+            'mensagem.max' => 'A mensagemd deve ter no máximo 2000 caracteres',
+            'nome.min' => 'O campo precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+            'required' => 'O campo :attribute deve ser preenchido',
+            'telefone.required' => 'O campo telefone precisa ser preenchido'
+        ];
+        $request->validate($regras,$feedback 
+        );
         SiteContato::create($request->all());
         return redirect()->route('site.index');
     }
