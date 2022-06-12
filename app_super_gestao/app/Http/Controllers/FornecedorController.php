e26a851d414b6cb06c6341c1cc9face5aef7d6ee
Vinicius <fsvinicius9@gmail.com>
2022-06-12 13:20:10 -0300

    Correção listar blade php

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -12,9 +12,15 @@ class FornecedorController extends Controller
         return view('app.fornecedor.index');
     }
 
-    public function listar()
+    public function listar(Request $request)
     {
-        return view('app.fornecedor.listar');
+        $fornecedores = Fornecedor::where('nome', 'like', '%'.$request->input('nome').'%')
+            ->where('site', 'like', '%'.$request->input('site').'%')
+            ->where('uf', 'like', '%'.$request->input('uf').'%')
+            ->where('email', 'like', '%'.$request->input('email').'%')
+            ->get();
+
+        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores]);
     }
 
     public function adicionar(Request $request)
