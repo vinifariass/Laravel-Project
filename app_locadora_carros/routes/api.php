05541bd9284943b90f58db5c649f6ca2a288098d
Vinicius <fsvinicius9@gmail.com>
2022-08-20 22:01:06 -0300

    Kernel and api configured

--- app_locadora_carros/routes/api.php
+++ app_locadora_carros/routes/api.php
@@ -17,14 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('v1')->middleware('jwt.auth')->roup(function () {
+    Route::apiResource('cliente', 'ClienteController');
+    Route::apiResource('carro', 'CarroController');
+    Route::apiResource('locacao', 'LocacaoController');
+    Route::apiResource('marca', 'MarcaController');
+    Route::apiResource('modelo', 'ModeloController');
+});
 
-Route::apiResource('cliente', 'ClienteController');
-Route::apiResource('carro', 'CarroController');
-Route::apiResource('locacao', 'LocacaoController');
-Route::apiResource('marca', 'MarcaController');
-Route::apiResource('modelo', 'ModeloController');
 
-Route::post('login','App\Http\Controllers\AuthController@login');
-Route::post('logout','App\Http\Controllers\AuthController@logout');
-Route::post('refresh','App\Http\Controllers\AuthController@refresh');
-Route::post('me','App\Http\Controllers\AuthController@me');
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('me', 'App\Http\Controllers\AuthController@me');
