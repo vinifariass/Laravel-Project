a30323aba9bb104622979cd59537044591fd9cb9
Vinicius <fsvinicius9@gmail.com>
2022-08-28 17:29:32 -0300

    Criação do cardcomponent

--- app_locadora_carros/resources/js/components/Marcas.vue
+++ app_locadora_carros/resources/js/components/Marcas.vue
@@ -1,51 +1,47 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card mb-3">
-          <div class="card-header">Busca de marcas</div>
-
-          <div class="card-body">
-            <div class="form-row">
-              <div class="col mb-3">
-                <input-container-component titulo="ID" id="inputId" id-help="idHelp"
-                  texto-ajuda="Opcional. Informe o ID da marca">
-                  <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                </input-container-component>
-              </div>
-              <div class="col mb-3">
-                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp"
-                  texto-ajuda="Opcional. Informe o nome da marca">
-                  <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
-                    placeholder="Nome da marca">
-                </input-container-component>
-
-              </div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <card-component titulo="Busca de marcas">
+        <template v-slot:conteudo>
+          <div class="form-row">
+            <div class="col mb-3">
+              <input-container-component titulo="ID" id="inputId" id-help="idHelp"
+                texto-ajuda="Opcional. Informe o ID da marca">
+                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+              </input-container-component>
+            </div>
+            <div class="col mb-3">
+              <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp"
+                texto-ajuda="Opcional. Informe o nome da marca">
+                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
+                  placeholder="Nome da marca">
+              </input-container-component>
 
             </div>
-          </div>
-
-          <div class="card-footer">
-            <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
 
           </div>
-        </div>
+        </template>
+        <template v-slot:rodape>
+
+          <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+
+        </template>
+      </card-component>
+      <div class="card mb-3">
+
         <!-- Fim card de busca -->
 
         <!-- Inicio card listagem de marcas -->
-
-        <div class="card">
-          <div class="card-header">Relação de marcas</div>
-
-          <div class="card-body">
-           <table-component></table-component>
-          </div>
-
-          <div class="card-footer">
+        <card-component titulo="Relação de marcas">
+          <template v-slot:conteudo>
+            <table-component></table-component>
+          </template>
+          <template v-slot:rodape>
             <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
 
-          </div>
-        </div>
+          </template>
+        </card-component>
+
       </div>
     </div>
   </div>
