fe99843156e3a0bdf84b47e016c7e30ba52250bb
Vinicius <fsvinicius9@gmail.com>
2022-09-03 10:50:05 -0300

    aplicando os dados filtrados no export default

--- app_locadora_carros/resources/js/components/Table.vue
+++ app_locadora_carros/resources/js/components/Table.vue
@@ -7,7 +7,11 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="obj in dados" :key="obj.id">
+      <tr>
+        <td v-for="obj, chave in dadosFiltrados" :key="chave"></td>
+        <td v-for="valor, chaveValor in obj" :key="chaveValor">{{ valor }}</td>
+      </tr>
+      <!-- <tr v-for="obj in dados" :key="obj.id">
         <td v-if="titulos.includes(chave)" v-for="valor, chave in obj " :key="chave">
           {{ chave }} - {{ valor }}
           <span v-if="chave == 'imagem'">
@@ -17,12 +21,12 @@
             {{ valor }}
           </span>
         </td>
-        <!-- <th scope="row">{{ m.id }}</th> -->
-        <!-- <td>{{ m.nome }}</td>
+         <th scope="row">{{ m.id }}</th> -->
+      <!-- <td>{{ m.nome }}</td>
         <td>{{ m.imagem }}</td>
         <td><img width="30" height="30" :src="/storage/ + m.imagem" alt="">
-        </td> -->
-      </tr>
+        </td> 
+      </tr> -->
 
     </tbody>
   </table>
@@ -30,6 +34,24 @@
 
 <script>
 export default {
-  props: ['dados']
+  props: ['dados'],
+  computed: {
+    dadosFiltrados() {
+      let campos = Object.keys(this.titulos)
+      let dadosFiltrados = []
+
+      this.dados.map((item, chave) => {
+        let itemFiltrado = {}
+        // console.log(chave, item)
+        campos.forEach(campo => {
+
+          itemFiltrado[campo] = item[campo] //sintaxe de array apara atribuir valores a objetos
+
+        })
+        dadosFiltrados.push(itemFiltrado)
+      })
+      return dadosFiltrados
+    }
+  }
 }
 </script>
